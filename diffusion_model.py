0123122a761b416b73f17e93dcaaf130b764c544
rmaptbu <rmaptbu@ucl.ac.uk>
2014-10-15 10:04:09 +0100

    made new python code for testing the testing

---
+++ diffusion_model.py
@@ -0,0 +1,21 @@
+def energy(density, D=1.0):
+	""" Energy associated with the diffusion model
+
+      Parameters
+      ----------
+
+      density: array of positive integers
+          Number of particles at each position i in the array
+      coeff: float
+          Diffusion coefficient.
+	"""
+	# implementation goes here
+  
+	E=0.0
+	for i in range(len(density)):
+		E += density[i]*density[i-1];
+	E*=D
+	print E
+	return E;
+	
+energy ([5.0, 6.0],1.0)
---
+++ test_diffusion_model.py
@@ -0,0 +1,6 @@
+from diffusion_model import energy
+def test_energy():
+  """ Optional description for nose reporting """
+  # Test something
+    
+  assert_equal(energy([1.0,1.0],1.0), 5.7)
