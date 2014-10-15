8affacb7677d1400c12b4cf24c3032f2001732f1
rmaptbu <rmaptbu@ucl.ac.uk>
2014-10-15 10:07:34 +0100

    added testing functionality

--- test_diffusion_model.py
+++ test_diffusion_model.py
@@ -2,5 +2,6 @@ from diffusion_model import energy
 def test_energy():
   """ Optional description for nose reporting """
   # Test something
-    
-  assert_equal(energy([1.0,1.0],1.0), 5.7)
+  
+  from nose.tools import assert_equal
+  assert_equal(energy([0.0,0.0],1.0), 0)
