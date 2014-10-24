7db0dc7e97f96f7d106662a6ed934b02d3b31639
Thore Buecking <rmaptbu@ucl.ac.uk>
2014-10-24 14:58:48 +0100

    designed random minimisation testing, function returns indexing errors

--- MCE.py
+++ MCE.py
@@ -13,18 +13,28 @@ def MonteCarlo(E_module, E_function,density,temperature=1):
 		
 		particle=randrange(0,len(temp_density))		#select random particle
 		if temp_density[particle] >= 1:				#move particle away if it exists
-			temp_density[particle]-=1							
-			if bool(random.getrandbits(1)):			#place particle to the left or right
+			temp_density[particle]-=1
+			if particle>0 and particle<len(temp_density):
+			#place particle randomly to the left or right if the outermost particle wasnt removed
+				if bool(random.getrandbits(1)):	
+					temp_density[particle+1]+=1
+				else:
+					temp_density[particle-1]+=1
+			#place particle to the left/right if last or first particle was removed respectively
+			elif particle==0:
 				temp_density[particle+1]+=1
 			else:
 				temp_density[particle-1]+=1
 		E1=Energy(temp_density)
 		
-		print "E0 is ", E0
-		print "E1 is ", E1
+		print "density0 is ", density
+		print "density1 is  ", temp_density
 		
 		if E1>E0 and exp(-(E1-E0)/temperature)<random.random(): #change passed
 			density=temp_density
+			print "step accepted"
+		else:
+			print "step not accepted"
 		
 	else:
 		raise NameError('Energy Module not loaded')
--- test_MCE.py
+++ test_MCE.py
@@ -4,7 +4,24 @@ def test_MonteCarlo():
 	from diffusion_model import energy	#use diffusion model energy function as test for functionality of MC minimisation
 	import nose.tools
 	from nose.tools import assert_equal
-	density=[1.0,1.0,1.0,1.0]
-	MonteCarlo ("diffusion_model","energy",density)
-	assert_equal(density,[1.0,1.0,1.0,1.0])
+	from random import randint
 	
+	#minimum energy state doesn't change at absolute zero
+	for n in range (10): #try ten times
+		density=[1.0,1.0,1.0,1.0]
+		MonteCarlo ("diffusion_model","energy",density)
+		assert_equal(density,[1.0,1.0,1.0,1.0])
+	
+	#energy state lowers until minimum energy state is reached at absolute zero
+	for n in range (10): #try ten times
+		for i in range (10):
+			density[i]=random.randint(1,10) #fill density with random integers
+		if max(density)-min(density)<=1:
+			density_backup=density[:]
+			MonteCarlo("diffusion_model","energy",density)
+			assert_true(density=density_backup)
+		else:
+			E0=energy(density)
+			while (max(density)-min(density)>=2): #if distribution is non-uniform energy minimum is not reached
+				MonteCarlo("diffusion_model","energy",density)
+		assert_true(E0>=energy(density))
